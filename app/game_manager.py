ac13b956cd5aafc8a3e3dac1934ebd998688d444
OculusMode <dhamejaprashant@gmail.com>
2021-07-13 15:56:33 +0530

    WIP: basic chess config

--- app/game_manager.py
+++ app/game_manager.py
@@ -1,8 +1,40 @@
+from copy import deepcopy
+
 from blessed import Terminal
 
 PIECES = "".join(chr(9812 + x) for x in range(12))
-ROW = ["A", "B", "C", "D", "E", "F", "G", "H"]
-COL = list(map(str, range(1, 9)))
+print(PIECES)
+ROW = ("A", "B", "C", "D", "E", "F", "G", "H")
+COL = tuple(map(str, range(1, 9)))
+
+# rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
+initial_game = [
+    ["r", "n", "b", "q", "k", "b", "n", "r"],
+    ["p"] * 8,
+    ["em"] * 8,
+    ["em"] * 8,
+    ["em"] * 8,
+    ["em"] * 8,
+    ["P"] * 8,
+    ["R", "N", "B", "Q", "K", "B", "N", "R"],
+]
+
+
+mapper = {
+    "em": ("", "white"),
+    "k": (PIECES[0], "white"),
+    "q": (PIECES[1], "white"),
+    "r": (PIECES[2], "white"),
+    "b": (PIECES[3], "white"),
+    "n": (PIECES[4], "white"),
+    "p": (PIECES[5], "white"),
+    "K": (PIECES[6], "black"),
+    "Q": (PIECES[7], "black"),
+    "R": (PIECES[8], "black"),
+    "B": (PIECES[9], "black"),
+    "N": (PIECES[10], "black"),
+    "P": (PIECES[11], "black"),
+}
 
 
 class Player:
@@ -29,13 +61,15 @@ class Game:
         self.players = None
         self.game_id = None  # the game lobby id that the server will provide for online multiplayer
         self.server_ip = None
-        self.chess_board = None
+        self.chess_board = deepcopy(initial_game)
         self.tile_width = 6
         self.tile_height = 3
         self.offset_x = 0
         self.offset_y = 0
         self.x = 0
         self.y = 0
+        # self.my_color = 'white' # for future
+        self.white_move = True  # this will chnage in multiplayer game
 
     # TODO:: IS THIS NEEDED?
     def __len__(self) -> int:
@@ -67,10 +101,8 @@ class Game:
             for i in range(x, x + self.tile_width):
                 with self.term.location(i, j):
                     print(style(" "))
-        with self.term.location(
-            x + (self.tile_width // 2) - 1, y + (self.tile_height // 2)
-        ):
-            print(style(text))
+        with self.term.location(x, y + (self.tile_height // 2)):
+            print(style(str.center(text, self.tile_width)))
 
     def show_game_screen(self) -> None:
         """Shows the chess board."""
@@ -85,30 +117,64 @@ class Game:
                 print(num)
             for i in range(len(self)):
                 if (i + j) % 2 == 0:
-                    fg = "black"
                     bg = "blue"
                 else:
-                    fg = "white"
                     bg = "green"
+                piece, color = mapper[self.chess_board[j][i]]
                 self.draw_tile(
                     x * 2 + i * (self.tile_width + self.offset_x),
                     j * (self.tile_height + self.offset_y),
-                    text=PIECES[0],
-                    fg=fg,
+                    text=piece,
+                    fg=color,
                     bg=bg,
                 )
         for i in range(len(self)):
             with self.term.location(
-                x * 2 + i * self.tile_width, len(self) * self.tile_height
+                x * 2 - 1 + i * self.tile_width, len(self) * self.tile_height
             ):
                 print(str.center(ROW[i], len(self)))
         print(self.term.move_y(self.term.height - 4))
-        print(self.term.black_on_blue(self.term.center("press the move")))
 
     def start_game(self) -> int:
         """Starts the chess game."""
-        pass
+        while True:
+            self.show_game_screen()
+            if self.white_move:
+                print(
+                    self.term.black_on_blue(
+                        self.term.center("Which piece you want to play white?")
+                    )
+                )
+            else:
+                print(
+                    self.term.black_on_blue(
+                        self.term.center("Which piece you want to play black?")
+                    )
+                )
+            # with self.term.cbreak():
+            inp = input()
+            # print(inp)
+            # TODO:: VALIDATION
+            row = ROW.index(inp[0].upper())
+            col = len(self) - int(inp[1])
+            # TODO:: VALIDATION
+            if self.chess_board[row][col] != "em":
+                inp2 = input()
+                if inp == inp2:
+                    print("LOL try again!")
+                    continue
+                # TODO:: VALIDATION
+                row2 = ROW.index(inp2[0].upper())
+                col2 = len(self) - int(inp2[1])
+                print(row, col, row2, col2)
+                piece = self.chess_board[row][col]
+                self.chess_board[row][col] = "em"
+                self.chess_board[row2][col2] = piece
+                # TODO:: UPDATE VIEW
+            else:
+                print("Invalid move")
+        return 1
 
 
 game = Game()
-game.show_game_screen()
+game.start_game()
